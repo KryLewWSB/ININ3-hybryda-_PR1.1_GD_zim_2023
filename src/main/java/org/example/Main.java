d6c1919053620d6927c14d2d6ae55e6d0f65334d
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 13:31:28 +0100

    20

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("Podaj wartość do sprawdzenia: [zatwierdź enterem]");
-            int odebranaWartosc = scan.nextInt();
-            System.out.println(odebranaWartosc % 5 == 0 ? "Liczba " + odebranaWartosc + " jest podzielna przez 5!" :
-                    "Liczba " + odebranaWartosc + " NIE jest podzielna przez 5!");
-
-            System.out.println("Czy kontynuować? [T / N]");
-            String czyKontynuowac = scan.next();
-            if(czyKontynuowac.equals("N")) break;
+        for(int i = 1; i <= 20; i++){
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
         }
+
 
     }
 }
