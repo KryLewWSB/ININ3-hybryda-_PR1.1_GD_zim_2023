79187c95f1b6ef12f291d8a92e2841f55f52cb3f
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 11:31:07 +0100

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int liczba = 5;
+        System.out.println("Liczba to: " + liczba);
     }
 }
