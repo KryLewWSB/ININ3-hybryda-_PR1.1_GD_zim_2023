02f675b22dc637e888e334423764748f56e19b06
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 11:42:34 +0100

    4if

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int liczba = 25;
+        //blok instrukcji warunkowych
         if(liczba > 10) {
-            System.out.println("Liczba większa od 10: " + liczba);
+            System.out.println("Liczba większa od 10: ");
+        }else if(liczba > 15) {
+            System.out.println("Liczba większa od 15: ");
+        }else if (liczba > 20){
+            System.out.println("Liczba większa od 20: ");
+        }else{
+            System.out.println("Liczba jest inna... ");
         }
 
-        if(liczba > 15) {
-            System.out.println("Liczba większa od 15: " + liczba);
-        }
+
     }
 }
