18062bc36d776389d8bef91d133b6b9c75f99242
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 12:17:50 +0100

    8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,21 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = -1, b = -11, c = 12;
 
-        double delta = Math.pow(b, 2) - 4 * a * c;  // (b * b) - 4 * a * c
-        System.out.println("Delta jest równa = " + delta);
+        Scanner scan = new Scanner(System.in);
 
-        if(delta < 0){
-            System.out.println("Brak miejsc zerowych (delta < 0)");
-        } else if (delta == 0) {
-            double x0 = -b / (2 * a);
-            System.out.println("Jedno miejsce zerowe delta = 0!\nx0 = " +x0);
-        } else if (delta > 0) {
-            double x1 = (-b - Math.sqrt(delta)) / (2 * a);
-            double x2 = (-b + Math.sqrt(delta)) / (2 * a);
-            System.out.println("Delta większa od 0!\nx1 = " + x1 +
-                               "\nx2 = " + x2);
+        System.out.println("Podaj liczbę do sprawdzenia: [Zatwierdź enterem]");
+        int input = scan.nextInt();
+        //Za pomocą instrukcji warunkowej:
+        if(input % 2 == 0){
+            System.out.println("Liczba " + input + " jest parzysta!");
+        }else{
+            System.out.println("Liczba " + input + " jest nieparzysta!");
         }
+
+
     }
 }
