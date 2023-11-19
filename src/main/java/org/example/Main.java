175f1f50a4063e573386f97ce1c3fb6fa823bf43
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 11:49:23 +0100

    5math

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int liczba = 25;
-        //blok instrukcji warunkowych
-        if(liczba > 10) {
-            System.out.println("Liczba większa od 10: ");
-        }else if(liczba > 15) {
-            System.out.println("Liczba większa od 15: ");
-        }else if (liczba > 20){
-            System.out.println("Liczba większa od 20: ");
-        }else{
-            System.out.println("Liczba jest inna... ");
-        }
 
+        int liczba2 = (int) Math.pow(2, 2);
+        double liczba3 = Math.pow(2, 2);
+        System.out.println("Zmienna int liczba2: " + liczba2 +
+                            "\nZmienna double liczba3: " + liczba3);
 
     }
 }
