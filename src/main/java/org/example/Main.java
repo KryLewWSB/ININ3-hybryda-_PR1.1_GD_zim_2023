737ec29bfc898e4e62b8b314872f66c4d6653127
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 11:39:20 +0100

    3if

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int liczba = 5;
-        System.out.println("Liczba to: " + liczba);
+        int liczba = 25;
+        if(liczba > 10) {
+            System.out.println("Liczba większa od 10: " + liczba);
+        }
+
+        if(liczba > 15) {
+            System.out.println("Liczba większa od 15: " + liczba);
+        }
     }
 }
