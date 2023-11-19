eb1c2b5d94f5e22ad47ac648acbd8f8dfc65d9d3
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 13:45:39 +0100

    23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,17 @@
 package org.example;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Początek - start
+
         Scanner scan = new Scanner(System.in);
-        //Podaj wartość a (operacja wejścia)
-        System.out.println("Podaj wartość a: ");
-        int a = scan.nextInt();
-        //Podaj wartość b (operacja wejścia)
-        System.out.println("Podaj wartość b: ");
-        int b = scan.nextInt();
-        //Podaj wartość c (operacja wejścia)
-        System.out.println("Podaj wartość c: ");
-        int c = scan.nextInt();
+        scan.useLocale(Locale.US);//kodowanie znaków US
+        System.out.println("Podaj wartość float: ");
+        float liczba = scan.nextFloat();
 
-        //Blok operacji
-       int suma = a + b + c;
-
-       //Blok wyjścia z programu
-       System.out.println(suma);
-        //KONIEC
-
+        System.out.println(liczba);
 
     }
 }
