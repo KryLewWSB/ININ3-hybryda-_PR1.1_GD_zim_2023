37e93c90978afaa7bca92dd208bc17add9ed5e8f
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 12:20:25 +0100

    10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,9 @@ public class Main {
         System.out.println("Podaj liczbę do sprawdzenia: [Zatwierdź enterem]");
         int input = scan.nextInt();
         //Za pomocą operatora warunkowego:
-        System.out.println( input % 2 == 0 ? "Liczba " + input + " jest parzysta!" : "Liczba " + input + " jest nieparzysta!");
+        System.out.println( input % 2 == 0 ?
+                "Liczba " + input + " jest parzysta!" :
+                "Liczba " + input + " jest nieparzysta!");
 
 
 
