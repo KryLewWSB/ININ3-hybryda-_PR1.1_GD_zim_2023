052ce51b17f27c06e7f67d1ebbec84868465eee2
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 13:32:02 +0100

    21

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         for(int i = 1; i <= 20; i++){
-            if(i % 2 == 0) {
-                System.out.println(i);
-            }
+            if(i % 2 == 0) System.out.println(i);
         }
 
 
