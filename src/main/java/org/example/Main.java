04571c7773483fdfbc04f043860d8602ad6603cd
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 17:04:59 +0100

    32

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,51 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        /* Przykład - gra w kości. */
+        /* Przykład - symulacja gry w lotto. */
         Scanner scan = new Scanner(System.in);
 
-        while(true) {
-            System.out.println("Podaj liczbę (1 - 6): ");
-            int liczbaUzytkownika = scan.nextInt();
 
-            int wartoscWylosowana = (int) (Math.random() * 5) + 1; //generowanie wartości pseudolosowej w zakresie 1-6
-
-            if (liczbaUzytkownika == wartoscWylosowana) {
-                System.out.println("Brawo! Zgadłeś wylosowaną liczbę oczek!\nTwój wybór: " +
-                        liczbaUzytkownika + "\nWylosowana wartość: " + wartoscWylosowana);
-            } else {
-                System.out.println("Niestety! Nie zgadłeś wylosowanej liczby oczek!\nTwój wybór: " +
-                        liczbaUzytkownika + "\nWylosowana wartość: " + wartoscWylosowana);
+        /* Start - Przyjmowanie 6 unikalnych liczb od użytkownika */
+        Set<Integer> liczbyPodanePrzezUzytkownika = new HashSet<>();
+        while (liczbyPodanePrzezUzytkownika.size() < 6){
+            System.out.println("Podaj liczbę: ");
+            int podanaLiczba = scan.nextInt();
+            if(podanaLiczba < 50 && podanaLiczba > 0 && !liczbyPodanePrzezUzytkownika.contains(podanaLiczba)){
+                liczbyPodanePrzezUzytkownika.add(podanaLiczba);
+            }else{
+                System.out.println("Podałeś błędną liczbę, podaj inną...");
             }
         }
+        /* Stop - Przyjmowanie 6 unikalnych liczb od użytkownika */
+        System.out.println(liczbyPodanePrzezUzytkownika);
+
+        /* Start - Losowanie 6 unikalnych liczb z metody random */
+        Set<Integer> listaWylosowanychLIczb = new HashSet<>();
+        while (listaWylosowanychLIczb.size() < 6){
+            int wylosowanaLIczba = (int) (Math.random() * 48) + 1;
+            if(!listaWylosowanychLIczb.contains(wylosowanaLIczba)){
+                listaWylosowanychLIczb.add(wylosowanaLIczba);
+            }
+        }
+        /* Stop - Losowanie 6 unikalnych liczb z metody random */
+        System.out.println(listaWylosowanychLIczb);
+
+        Set<Integer> liczbyTrafione = new HashSet<>();
+        for(int liczba : listaWylosowanychLIczb){
+            if(liczbyPodanePrzezUzytkownika.contains(liczba)){
+                liczbyTrafione.add(liczba);
+            }
+        }
+
+        System.out.println("Trafione liczby: " + liczbyTrafione);
+
+        //int wartoscWylosowana = (int) (Math.random() * 48) + 1; //1 - 49
+
+
+
 
     }
 }
