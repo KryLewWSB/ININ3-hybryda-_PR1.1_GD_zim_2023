6c0daeb34eecb59a04d7844dfa3dd7f5dd201d1d
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 16:35:06 +0100

    31

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /* Zadanie 11 */
+        /* Przykład - gra w kości. */
         Scanner scan = new Scanner(System.in);
-        System.out.println("Podaj liczbę: ");
-        int liczba = scan.nextInt();
 
-        if(liczba > 0){
-            System.out.println("Liczba to: " + liczba);
-        }else if (liczba < 0){
-            System.out.println("Liczba to: " + Math.abs(liczba));
+        while(true) {
+            System.out.println("Podaj liczbę (1 - 6): ");
+            int liczbaUzytkownika = scan.nextInt();
+
+            int wartoscWylosowana = (int) (Math.random() * 5) + 1; //generowanie wartości pseudolosowej w zakresie 1-6
+
+            if (liczbaUzytkownika == wartoscWylosowana) {
+                System.out.println("Brawo! Zgadłeś wylosowaną liczbę oczek!\nTwój wybór: " +
+                        liczbaUzytkownika + "\nWylosowana wartość: " + wartoscWylosowana);
+            } else {
+                System.out.println("Niestety! Nie zgadłeś wylosowanej liczby oczek!\nTwój wybór: " +
+                        liczbaUzytkownika + "\nWylosowana wartość: " + wartoscWylosowana);
+            }
         }
 
     }
