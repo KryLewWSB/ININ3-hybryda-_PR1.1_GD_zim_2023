9e853fa0ee89dcd522a44ea01b8f7149dac15d77
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 15:13:37 +0100

    24

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        scan.useLocale(Locale.US);//kodowanie znaków US
-        System.out.println("Podaj wartość float: ");
-        float liczba = scan.nextFloat();
+        int[] tablicaLiczba = new int[5]; //deklaracja tablicy intów o długości 5
 
-        System.out.println(liczba);
+        tablicaLiczba[2] = 5;
 
+        // 1 sposób wypisania tablicy:
+        for(int liczba : tablicaLiczba){
+            System.out.println(liczba);
+        }
+
+        System.out.println( Arrays.toString( tablicaLiczba ) ); //2 sposób wypisania
     }
 }
